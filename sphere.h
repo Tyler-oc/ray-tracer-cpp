9c2e49ded4dd055e9b8e62485a006edb2ef47d1e
Tyler Onstad-Cran <tonstadcran@gmail.com>
2025-11-10 19:34:38 -0700

    sphere and hittable

---
+++ sphere.h
@@ -0,0 +1,35 @@
+#ifndef SPHERE_H
+#define SPHERE_H
+
+#include "hittable.h"
+#include "vec3.h"
+
+class sphere : public hittable
+{
+public:
+    sphere(const point3 &center, double radius) : center(center), radius(std::fmax(0, radius)) {}
+
+    bool hit(const ray &r, double ray_tmin, double ray_tmax, hit_record &hit) const override
+    {
+        vec3 oc = center - r.origin();
+        auto a = r.direction().length_squared();
+        auto h = dot(r.direction(), oc);
+        auto c = oc.length_squared() - radius * radius;
+        auto discriminant = h * h - a * c;
+
+        if (discriminant < 0)
+        {
+            return false;
+        }
+
+        auto sqrtd = std::sqrt(discriminant);
+
+        // find nearest root in acceptable range
+    }
+
+private:
+    point3 center;
+    double radius;
+}
+
+#endif
