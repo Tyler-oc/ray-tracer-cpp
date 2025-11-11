86eed28f126f0f4b94f3228125bf24639539dc43
Tyler Onstad-Cran <tonstadcran@gmail.com>
2025-11-10 21:54:02 -0700

    hittables and hittable lists

--- sphere.h
+++ sphere.h
@@ -9,7 +9,7 @@ class sphere : public hittable
 public:
     sphere(const point3 &center, double radius) : center(center), radius(std::fmax(0, radius)) {}
 
-    bool hit(const ray &r, double ray_tmin, double ray_tmax, hit_record &hit) const override
+    bool hit(const ray &r, double ray_tmin, double ray_tmax, hit_record &rec) const override
     {
         vec3 oc = center - r.origin();
         auto a = r.direction().length_squared();
@@ -24,12 +24,28 @@ public:
 
         auto sqrtd = std::sqrt(discriminant);
 
-        // find nearest root in acceptable range
+        // find nearest root in acceptable range, both + and -
+        auto root = (h - sqrtd) / a;
+        if (root <= ray_tmin || ray_tmax <= root)
+        {
+            root = (h + sqrtd) / a;
+            if (root <= ray_tmin || ray_tmax <= root)
+            {
+                return false;
+            }
+        }
+
+        rec.t = root;
+        rec.p = r.at(rec.t);
+        vec3 outward_normal = (rec.p - center) / radius;
+        rec.set_face_normal(r, outward_normal);
+
+        return true;
     }
 
 private:
     point3 center;
     double radius;
-}
+};
 
 #endif
