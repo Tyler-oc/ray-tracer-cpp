cf1376e667f227ac350a0f8d329c3e82ea388f5f
Tyler Onstad-Cran <tonstadcran@gmail.com>
2025-11-19 20:21:44 -0700

    short and sweet way to end a tired day

---
+++ material.h
@@ -0,0 +1,19 @@
+#ifndef MATERIAL_H
+#define MATERIAL_H
+
+#include "hittable.h"
+#include "ray.h"
+#include "color.h"
+
+class material
+{
+public:
+    virtual ~material() = default;
+
+    virtual bool scatter(const ray &ray_in, const hit_record &rec, color &attenuation, ray &scattered) const
+    {
+        return false;
+    }
+};
+
+#endif
